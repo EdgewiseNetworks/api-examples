a3284dddcbf92be7b7d00689ce02f42f8f9278f3
Dan Perkins <dperkins@edgewise.net>
2019-11-06 08:47:34 -0500

    Example showing how to add an app to an app collection. Includes new edgeutils lib and site config file.

---
+++ v1/python/add-app-to-collection.py
@@ -0,0 +1,137 @@
+#!/usr/bin/python
+#
+# Copyright 2019 Edgewise Networks
+# SPDX-License-Identifier: Apache-2.0
+#
+
+import yaml
+import argparse
+from edgeutils import ApiSession
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--collection_name', required=True,
+                    help='Name of the app collection to modify.')
+parser.add_argument('--app_name', required=True,
+                    help='Name of the app to add to the collection.')
+parser.add_argument('--app_hash',
+                    help='SHA256 hash of the app to add to the collection.')
+parser.add_argument('--simulated', action='store_true',
+                    help='Actions are simulated. No changes are made.')
+parser.add_argument('--force', action='store_true',
+                    help='Force a re-evalution of the collection membership.')
+args = parser.parse_args()
+
+with open('config.yaml') as f:
+    config = yaml.safe_load(f)
+
+api = ApiSession(config)
+
+
+#### New app checks ####
+
+# Check for matching apps
+url = 'scopes/inbound/scope-stats?appNames={}&scopeType=HOST_APP'.format(args.app_name)
+apps = api.get(url)['content']
+if not apps:
+    raise Exception("No app instances found for name: {}".format(args.app_name))
+
+app_hashes = {}
+for app in apps:
+    id = app['scope']['id'].split(':', 1)[-1]
+    features = api.get('apps/{}'.format(id))
+    app_hashes[id] = features['features']['FILE_SHA256']
+
+# Check that if a hash is provided, it is known to Edgewise
+if args.app_hash:
+    if args.app_hash not in app_hashes.values():
+        raise Exception("App SHA256 not indexed. \
+                         Ensure app has run on an Edgewise managed host.")
+
+
+#### Collection checks ####
+
+# Check for matching collection
+collections = [x for x in api.get('collections')['content'] if x['name'] == args.collection_name]
+if not collections:
+    raise Exception("No collection found with name: {}".format(args.collection_name))
+collection = collections[0]
+
+# Check if collection is user managed
+if collection['owner'] != 'USER':
+    raise Exception("Collection is not user owned")
+
+# Check if collection type is app collection
+if collection['query']['type'] != 'HOST_APP':
+    raise Exception("Collection is not type app collection")
+
+# Check if app is already in the collection
+if args.app_name in collection['query']['appNames']:
+    print("App is already a member of collection: {}".format(args.collection_name))
+    if args.force:
+        print("Force flag used. Continuing...")
+    else:
+        print("Use --force to override")
+        exit()
+
+
+#### Review current collection apps ####
+
+url = 'collections/{}/scopes/inbound/scope-stats'.format(collection['id'])
+scopes = api.get(url)['content']
+
+# Build the list of collection apps before making any changes
+apps_before = []
+for scope in scopes:
+    id = scope['scope']['id'].split(':',1)[-1]
+    features = api.get('apps/{}'.format(id))
+    hash = features['features']['FILE_SHA256']
+    apps_before.append('{}:{}'.format(id, hash))
+
+
+#### Update collection ####
+
+current = collection['query']['appNames']
+collection['query']['appNames'] = current + [args.app_name]
+if not args.simulated:
+    api.put('collections/{}'.format(collection['id']), collection)
+
+
+#### Compare collection apps before-after ####
+
+if not args.simulated:
+    # Get a fresh collection app list
+    url = 'collections/{}/scopes/inbound/scope-stats'.format(collection['id'])
+    scopes = api.get(url)['content']
+else:
+    # No point in querying the API again, collection
+    # membership is not changed in simulated mode
+    scopes = apps
+
+# Build the list of collection apps that were added or simulated added
+apps_after = []
+for scope in scopes:
+    id = scope['scope']['id'].split(':',1)[-1]
+    features = api.get('apps/{}'.format(id))
+    hash = features['features']['FILE_SHA256']
+    apps_after.append('{}:{}'.format(id, hash))
+
+# Check for no-op
+if apps_before == apps_after:
+    print("Collection apps list before and after match. No change was made.")
+    exit()
+
+# Do a detailed before-after comparison
+app_diff = [x for x in apps_after if x not in apps_before]
+if app_diff:
+    for app in app_diff:
+        id, hash = app.split(':', 1)
+        features = api.get('apps/{}'.format(id))
+        print("Added app instance")
+        print("  App name: {}".format(features['name']))
+        print("  SHA256: {}".format(hash))
+        if args.app_hash and hash != args.app_hash:
+            print("\nWARNING: hash of app added does not match provided app_hash. \
+                   Manually review colleciton membership.\n")
+        print()
+else:
+    print("Collection app membership diff shows no change")
---
+++ v1/python/edgeutils.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python
+#
+# Copyright 2019 Edgewise Networks
+# SPDX-License-Identifier: Apache-2.0
+#
+
+import requests
+from urllib.parse import urlparse
+
+class ApiSession:
+    def __init__(self, config):
+        self.site_id = config['site_id']
+        self.username = config['username']
+        self.password = config['password']
+        self.cert = (config['cert_file'], config['key_file'])
+        self.url_root = 'https://console.edgewise.services'
+        self.url_auth = self.url_root + '/auth/login'
+        self.url_api = self.url_root + '/api/v1/sites/{}'.format(self.site_id)
+        self.access_token = self.authenticate()
+        self.headers = {'authorization':'Bearer %s' % self.access_token}
+
+    def authenticate(self):
+        response = requests.post(
+            url=self.url_auth,
+            json={'username':self.username, 'password':self.password},
+            cert=self.cert,
+        )
+        response.raise_for_status()
+        return response.json()['accessToken']
+
+    def get(self, url):
+        if not self._is_url(url):
+            url = self.url_api + '/{}'.format(url)
+        response = requests.get(
+            url=url,
+            headers=self.headers,
+            cert=self.cert,
+        )
+        response.raise_for_status()
+        return response.json()
+
+    def put(self, url, payload):
+        if not self._is_url(url):
+            url = self.url_api + '/{}'.format(url)
+        response = requests.put(
+            url=url,
+            headers=self.headers,
+            json=payload,
+            cert=self.cert,
+        )
+        response.raise_for_status()
+        return response.json()
+
+    def post(self, url, payload):
+        if not self._is_url(url):
+            url = self.url_api + '/{}'.format(url)
+        response = requests.post(
+            url=url,
+            headers=self.headers,
+            json=payload,
+            cert=self.cert,
+        )
+        response.raise_for_status()
+        return response.json()
+
+    def delete(self, url):
+        if not self._is_url(url):
+            url = self.url_api + '/{}'.format(url)
+        response = requests.delete(
+            url=url,
+            headers=self.headers,
+            cert=self.cert,
+        )
+        response.raise_for_status()
+        return response
+
+    def _is_url(self, url):
+      try:
+        result = urlparse(url)
+        return all([result.scheme, result.netloc])
+      except ValueError:
+        return False

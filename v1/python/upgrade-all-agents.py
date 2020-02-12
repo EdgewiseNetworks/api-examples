9d338e7742097ad1a18246165f3364291818035f
Dan Perkins <dperkins@edgewise.net>
2020-02-12 12:43:18 -0500

    Update upgrade script to use edgeutils, add new script for upgrading by agents segment

--- v1/python/upgrade-all-agents.py
+++ v1/python/upgrade-all-agents.py
@@ -4,59 +4,32 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-import requests
-
-site_id = ''
-username = ''
-password = ''
-cert_file = '/path/to/cert.pem'
-key_file = '/path/to/key.pem'
+import yaml
+from edgeutils import ApiSession
 
 agent_operation = 'UPGRADE'
 
-# Authenticate
-response = requests.post(
-    url='https://console.edgewise.services/auth/login',
-    json={'username': username, 'password': password},
-    cert=(cert_file, key_file),
-)
-response.raise_for_status()
-access_token = response.json()['accessToken']
+with open('config.yaml') as f:
+    config = yaml.safe_load(f)
+
+api = ApiSession(config)
 
 # Get agents info
-response = requests.get(
-    url='https://console.edgewise.services/api/v1/sites/{}/agents'.format(site_id),
-    headers={'authorization': 'Bearer %s' % access_token},
-    cert=(cert_file, key_file),
-)
-response.raise_for_status()
-agents = [{'id': x['id'], 'name':x['name']} for x in response.json()]
+r = api.get('agents')
+agents = [{'id': x['id'], 'name':x['name'], 'status':x['status']} for x in r]
 
 # Prepare agents for operation
 if agent_operation in ['UPGRADE', 'UNINSTALL']:
     for agent in agents:
-        try:
-            response = requests.put(
-                url='https://console.edgewise.services/api/v1/sites/{}/agents/{}/config'.format(site_id, agent['id']),
-                headers={'authorization': 'Bearer %s' % access_token},
-                json={'configOptions': [{'name': 'AgentTerminationProtection', 'value': 0, 'type': 'int'}]},
-                cert=(cert_file, key_file),
-            )
-            response.raise_for_status()
-        except:
-            print("Failed to prepare agent on host '{}' for operation '{}', skipping...".format(agent['name'], agent_operation))
-            agents[:] = [x for x in agents if x.get('id') != agent['id']]
+        if agent['status'] == 'CONNECTED':
+            payload = {'configOptions': [{'name': 'AgentTerminationProtection', 'value': 0, 'type': 'int'}]}
+            api.put('agents/{}/config'.format(agent['id']), payload)
+            print("Disabled termination protection on host '{}'".format(agent['name']))
 
 # Perform operation
 for agent in agents:
-    print("Running operation '{}' for agent on host '{}'".format(agent_operation, agent['name']))
-    try:
-        response = requests.post(
-            url='https://console.edgewise.services/api/v1/sites/{}/agents/{}/operation'.format(site_id, agent['id']),
-            headers={'authorization': 'Bearer %s' % access_token},
-            json={'siteId': site_id, 'name': agent['name'], 'targetId': agent['id'], 'operation': agent_operation},
-            cert=(cert_file, key_file),
-        )
-        response.raise_for_status()
-    except:
-        print("Failed operation '{}' for agent on host '{}'".format(agent_operation, agent['name']))
+    if agent['status'] == 'CONNECTED':
+        payload = {'siteId': config['site_id'], 'name': agent['name'], 'targetId': agent['id'], 'operation': agent_operation}
+        api.post('agents/{}/operation'.format(agent['id']), payload)
+        print("Successfully ran '{}' command on host '{}'".format(agent_operation, agent['name']))
+
---
+++ v1/python/upgrade-segment-agents.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+#
+# Copyright 2020 Edgewise Networks
+# SPDX-License-Identifier: Apache-2.0
+#
+
+import yaml
+import argparse
+from edgeutils import ApiSession
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--segment_name', required=True,
+                    help='Name of the segment collection to modify.')
+args = parser.parse_args()
+
+agent_operation = 'UPGRADE'
+
+with open('config.yaml') as f:
+    config = yaml.safe_load(f)
+
+api = ApiSession(config)
+
+# Get segments
+collections = api.get('collections')['content']
+user_collections = [x for x in collections if x['owner'] == 'USER']
+segments = [x for x in user_collections if x['query']['type'] == 'HOST']
+
+# Verify that the supplied segment exists
+segment_names = [x['name'] for x in segments]
+if args.segment_name not in segment_names:
+    raise Exception("Supplied segment '{}' was not found. Valid segments are: \n{}".format(
+        args.segment_name, 
+        segment_names
+    ))
+
+# Find the correct segment
+segment = next((x for x in segments if x['name'] == args.segment_name), None)
+if not segment:
+    raise Exception('Failed to get required information about the supplied host segment')
+
+# Get segment hosts
+r = api.get('scopes/{}/scope-data?scopeType=COLLECTION'.format(segment['id']))
+hosts = [x['name'] for x in r['childScopes'] if x['type'] == 'HOST']
+
+# Get agents for segment hosts
+r = api.get('agents')
+agents = [{'id': x['id'], 'name':x['name'], 'status':x['status']} for x in r if x['name'] in hosts]
+
+# Prepare agents for operation
+if agent_operation in ['UPGRADE', 'UNINSTALL']:
+    for agent in agents:
+        if agent['status'] == 'CONNECTED':
+            payload = {'configOptions': [{'name': 'AgentTerminationProtection', 'value': 0, 'type': 'int'}]}
+            api.put('agents/{}/config'.format(agent['id']), payload)
+            print("Disabled termination protection on host '{}'".format(agent['name']))
+
+# Perform operation
+for agent in agents:
+    if agent['status'] == 'CONNECTED':
+        payload = {'siteId': config['site_id'], 'name': agent['name'], 'targetId': agent['id'], 'operation': agent_operation}
+        api.post('agents/{}/operation'.format(agent['id']), payload)
+        print("Successfully ran '{}' command on host '{}'".format(agent_operation, agent['name']))
+

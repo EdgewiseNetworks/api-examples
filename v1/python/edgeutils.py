64be584c4f0ac462cdf6b1b1cfb2958a27a2419c
Dan Perkins <dperkins@zscaler.com>
2021-10-26 10:22:31 -0400

    Update edgeutils lib to add support for retrying REST calls

--- v1/python/edgeutils.py
+++ v1/python/edgeutils.py
@@ -7,27 +7,22 @@
 import requests
 from getpass import getpass
 from urllib.parse import urlparse
+from urllib3.util.retry import Retry
+from requests.adapters import HTTPAdapter
 
 class ApiSession:
     def __init__(self, config):
         self.site_id = config['site_id']
         self.username = self._get_username(config)
         self.password = self._get_password(config)
-        self.cert = (config['cert_file'], config['key_file'])
+
         self.url_root = config['url_root']
         self.url_auth = self.url_root + '/auth/login'
         self.url_api = self.url_root + '/api/v1/sites/{}'.format(self.site_id)
-        self.access_token = self.authenticate()
-        self.headers = {'authorization':'Bearer %s' % self.access_token}
 
-    def authenticate(self):
-        response = requests.post(
-            url=self.url_auth,
-            json={'username':self.username, 'password':self.password},
-            cert=self.cert,
-        )
-        response.raise_for_status()
-        return response.json()['accessToken']
+        self.session = self._session()
+        self.session.cert = (config['cert_file'], config['key_file'])
+        self.session.headers.update({'authorization':'Bearer %s' % self._authenticate()})
 
     def get(self, url, page=0, size=10000):
         if not self._is_url(url):
@@ -35,24 +30,15 @@ class ApiSession:
                 separator = '&'
             else:
                 separator = '?'
-            url = self.url_api + '/{}{}page={}&size={}'.format(url, separator, page, size)
-        response = requests.get(
-            url=url,
-            headers=self.headers,
-            cert=self.cert,
-        )
+            url = self.url_api + '/{}{}page={}&size={}'.format(url, separator, page, size)        
+        response = self.session.get(url=url)
         response.raise_for_status()
         return response.json()
 
     def put(self, url, payload):
         if not self._is_url(url):
             url = self.url_api + '/{}'.format(url)
-        response = requests.put(
-            url=url,
-            headers=self.headers,
-            json=payload,
-            cert=self.cert,
-        )
+        response = self.session.put(url=url, json=payload)
         response.raise_for_status()
         if not response.text:
             return
@@ -61,12 +47,7 @@ class ApiSession:
     def post(self, url, payload):
         if not self._is_url(url):
             url = self.url_api + '/{}'.format(url)
-        response = requests.post(
-            url=url,
-            headers=self.headers,
-            json=payload,
-            cert=self.cert,
-        )
+        response = self.session.post(url=url, json=payload)
         response.raise_for_status()
         if not response.text:
             return
@@ -75,11 +56,7 @@ class ApiSession:
     def delete(self, url):
         if not self._is_url(url):
             url = self.url_api + '/{}'.format(url)
-        response = requests.delete(
-            url=url,
-            headers=self.headers,
-            cert=self.cert,
-        )
+        response = self.session.delete(url=url)
         response.raise_for_status()
         if not response.text:
             return
@@ -103,4 +80,21 @@ class ApiSession:
             return config['password']
         else:
             return str(getpass())
-        
+    
+    def _authenticate(self):
+        response = self.session.post(
+            url=self.url_auth,
+            json={'username':self.username, 'password':self.password},
+        )
+        response.raise_for_status()
+        return response.json()['accessToken']
+
+    def _session(self):
+        session = requests.Session()
+        retries = Retry(total=5,
+                        backoff_factor=0.1,
+                        status_forcelist=[500, 502, 503, 504])
+        session.mount('https://', HTTPAdapter(max_retries=retries))
+        session.mount('http://', HTTPAdapter(max_retries=retries))
+        return session
+

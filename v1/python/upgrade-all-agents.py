f6fb7b7335b0ceae0a997efeb9c17d3cc58cfb7e
Dan Perkins <dperkins@zscaler.com>
2022-03-28 14:31:01 -0400

    Add count arg option and check for upgrade available (#7)

--- v1/python/upgrade-all-agents.py
+++ v1/python/upgrade-all-agents.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
 #
-# Copyright 2019 Edgewise Networks
+# Copyright 2022 Zscaler
 # SPDX-License-Identifier: Apache-2.0
 #
 
 import yaml
+import argparse
 from requests.exceptions import HTTPError
 from edgeutils import ApiSession
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-c', '--count', required=False,
+                    help='Perform this count of upgrades and then exit.')
+args = parser.parse_args()
 
 agent_operation = 'UPGRADE'
 
@@ -17,23 +23,26 @@ api = ApiSession(config)
 
 # Get agents info
 r = api.get('agents')
-agents = [{'id': x['id'], 'name':x['name'], 'status':x['status']} for x in r]
+agents = [{'id': x['id'], 'name':x['name'], 'status':x['status']} for x in r if x['upgradePackage']]
 
-# Prepare agents for operation
 if agent_operation in ['UPGRADE', 'UNINSTALL']:
+    count = 0
     for agent in agents:
         if agent['status'] == 'CONNECTED':
-            payload = {'configOptions': [{'name': 'AgentTerminationProtection', 'value': 0, 'type': 'int'}]}
-            api.put('agents/{}/config'.format(agent['id']), payload)
-            print("Disabled termination protection on host '{}'".format(agent['name']))
-
-# Perform operation
-for agent in agents:
-    if agent['status'] == 'CONNECTED':
-        try:
-            payload = {'siteId': config['site_id'], 'name': agent['name'], 'targetId': agent['id'], 'operation': agent_operation}
-            api.post('agents/{}/operation'.format(agent['id']), payload)
-            print("Successfully ran '{}' command on host '{}'".format(agent_operation, agent['name']))
-        except HTTPError:
-            print("Failed to run '{}' command on host '{}'. Is it already upgraded?".format(agent_operation, agent['name']))
+            try:
+                # Disable termination protection
+                payload = {'configOptions': [{'name': 'AgentTerminationProtection', 'value': 0, 'type': 'int'}]}
+                api.put('agents/{}/config'.format(agent['id']), payload)
+                print("Disabled termination protection on host '{}'".format(agent['name']))
+                
+                # Perform operation
+                payload = {'siteId': config['site_id'], 'name': agent['name'], 'targetId': agent['id'], 'operation': agent_operation}
+                api.post('agents/{}/operation'.format(agent['id']), payload)
+                print("Successfully ran '{}' command on host '{}'".format(agent_operation, agent['name']))
+                count += 1
+            except HTTPError:
+                print("Failed to run '{}' command on host '{}'".format(agent_operation, agent['name']))
+            if args.count:
+                if count >= int(args.count):
+                    break
 

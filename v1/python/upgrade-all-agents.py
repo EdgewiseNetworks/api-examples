07d4a1eb50ee4b793dd5347c534601766394b7df
Dan Perkins <dperkins@zscaler.com>
2022-03-28 14:30:20 -0400

    Refactor

--- v1/python/upgrade-all-agents.py
+++ v1/python/upgrade-all-agents.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright 2019 Edgewise Networks
+# Copyright 2022 Zscaler
 # SPDX-License-Identifier: Apache-2.0
 #
 
@@ -25,25 +25,24 @@ api = ApiSession(config)
 r = api.get('agents')
 agents = [{'id': x['id'], 'name':x['name'], 'status':x['status']} for x in r if x['upgradePackage']]
 
-# Prepare agents for operation
 if agent_operation in ['UPGRADE', 'UNINSTALL']:
+    count = 0
     for agent in agents:
         if agent['status'] == 'CONNECTED':
-            payload = {'configOptions': [{'name': 'AgentTerminationProtection', 'value': 0, 'type': 'int'}]}
-            api.put('agents/{}/config'.format(agent['id']), payload)
-            print("Disabled termination protection on host '{}'".format(agent['name']))
-
-# Perform operation
-count = 0
-for agent in agents:
-    if agent['status'] == 'CONNECTED':
-        try:
-            payload = {'siteId': config['site_id'], 'name': agent['name'], 'targetId': agent['id'], 'operation': agent_operation}
-            api.post('agents/{}/operation'.format(agent['id']), payload)
-            print("Successfully ran '{}' command on host '{}'".format(agent_operation, agent['name']))
-            count += 1
-        except HTTPError:
-            print("Failed to run '{}' command on host '{}'. Is it already upgraded?".format(agent_operation, agent['name']))
-        if count >= args.count:
-                break
+            try:
+                # Disable termination protection
+                payload = {'configOptions': [{'name': 'AgentTerminationProtection', 'value': 0, 'type': 'int'}]}
+                api.put('agents/{}/config'.format(agent['id']), payload)
+                print("Disabled termination protection on host '{}'".format(agent['name']))
+                
+                # Perform operation
+                payload = {'siteId': config['site_id'], 'name': agent['name'], 'targetId': agent['id'], 'operation': agent_operation}
+                api.post('agents/{}/operation'.format(agent['id']), payload)
+                print("Successfully ran '{}' command on host '{}'".format(agent_operation, agent['name']))
+                count += 1
+            except HTTPError:
+                print("Failed to run '{}' command on host '{}'".format(agent_operation, agent['name']))
+            if args.count:
+                if count >= int(args.count):
+                    break
 
